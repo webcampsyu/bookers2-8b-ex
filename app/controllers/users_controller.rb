64dc406554c2f2d0f8af23315ebbc668817988ff
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-06-09 06:36:37 +0000

    [save]投稿数(今週、先週)実装完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
     @book = Book.new
     @today_book = @books.created_today
     @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
   end
 
   def index
--- app/models/book.rb
+++ app/models/book.rb
@@ -8,8 +8,10 @@ class Book < ApplicationRecord
   #scope :スコープ名, -> { 条件式 }
   #スコープ名で条件式を呼び出せる
   #where(条件)でほしいデータを取り出す
-  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
-  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } #今日
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } #前日
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } #今週
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } #先週
   
   #favorited_by?メソッドで引数で渡されたユーザidがfavoritesテーブル内に
   #存在するかどうかを調べる
